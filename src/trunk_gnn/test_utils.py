ab9a330d7bf3dd69fd68b4c73b0c9b40bc3a682f
Patrick Benito <peberhard@ethz.ch>
2025-03-04 21:25:34 -0800

    add velocity plots

--- src/trunk_gnn/test_utils.py
+++ src/trunk_gnn/test_utils.py
@@ -63,13 +63,43 @@ def plot_rollout(states: torch.Tensor, states_gt: torch.Tensor, links = None):
 
     return fig
 
+def plot_velocities(states: torch.Tensor, states_gt: torch.Tensor, links=None):
+    if links is None:
+        links = [-1]
+
+    fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+
+    states = states.detach().cpu().numpy()
+    states_gt = states_gt.detach().cpu().numpy()
+
+    for i in links:
+        ax[0].plot(states[:, i, 3], label=f'Prediction $v_x{i}$')
+        ax[0].plot(states_gt[:, i, 3], label=f'Ground Truth $v_x{i}$')
+        ax[1].plot(states[:, i, 4], label=f'Prediction $v_y{i}$')
+        ax[1].plot(states_gt[:, i, 4], label=f'Ground Truth $v_y{i}$')
+        ax[2].plot(states[:, i, 5], label=f'Prediction $v_z{i}$')
+        ax[2].plot(states_gt[:, i, 5], label=f'Ground Truth $v_z{i}$')
+
+    ax[0].set_xlabel('Time')
+    ax[0].set_ylabel('Velocity $x$')
+    ax[0].legend()
+    ax[1].set_xlabel('Time')
+    ax[1].set_ylabel('Velocity $y$')
+    ax[1].legend()
+    ax[2].set_xlabel('Time')
+    ax[2].set_ylabel('Velocity $z$')
+    ax[2].legend()
+
+    return fig
+
 
 def open_loop_test(model: torch.nn.Module, test_data_loader: Data, additonal_info: str = ""):
     states, gt_states = test_rollout(model, list(test_data_loader))
     rmse = open_loop_link_rmse(states, gt_states)
-    fig = plot_rollout(states, gt_states, links=[0, 10, 20, -1])
+    fig_positions = plot_rollout(states, gt_states, links=[0, 10, 20, -1])
+    fig_velocities = plot_velocities(states, gt_states, links=[0, 10, 20, -1])
     
-    wandb.log({f"open_loop_rmse_{additonal_info}": rmse.item(), f"open_loop_rollout_fig_{additonal_info}": wandb.Image(fig)})
+    wandb.log({f"open_loop_rmse_{additonal_info}": rmse.item(), f"open_loop_rollout_fig_positions_{additonal_info}": wandb.Image(fig_positions), f"open_loop_rollout_fig_velocities_{additonal_info}": wandb.Image(fig_velocities)})
 
     return rmse
 
