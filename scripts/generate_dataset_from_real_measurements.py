408d0cf96e7291ea3bfa5b485e32847a873d72a6
Patrick Benito <peberhard@ethz.ch>
2025-05-13 23:51:14 -0700

    generate dataset from measurements with trunk filter

--- scripts/generate_dataset_from_real_measurements.py
+++ scripts/generate_dataset_from_real_measurements.py
@@ -5,13 +5,9 @@ import re
 import argparse
 from tqdm import tqdm
 
-def create_dataframe_from_folder(folder):
-    states = pd.read_csv(os.path.join(folder, "states.csv"))
-    inputs = pd.read_csv(os.path.join(folder, "inputs.csv"))
+from trunk_filter.composite_filter import NonCausalTrunkFilter
+from trunk_filter.utils import integrate_positions_from_velocity, get_data_array_from_dataframe, get_dataframe_from_data_array
 
-    data = pd.merge(states, inputs, on="ID")
-
-    return data
 
 input_mapping_from_real_to_sim = {
     "phi2": "ux3",
@@ -21,6 +17,45 @@ input_mapping_from_real_to_sim = {
     "phi3": "ux1",
     "phi5": "uy1",
 }
+
+
+def create_dataframe_from_folder(folder):
+    states = pd.read_csv(os.path.join(folder, "states.csv"))
+    inputs = pd.read_csv(os.path.join(folder, "inputs.csv"))
+
+    data = pd.merge(states, inputs, on="ID")
+    data = add_virtual_node_at_origin(data)
+    data['t'] = data['ID'] * 0.01
+    data = rename_columns(data)
+
+    return data
+
+
+def split_test_data(data):
+    segments = []
+    current_segment = []
+
+    count = 0
+    num_test_files = 0
+    for _, row in data.iterrows():
+        if count > 1000:
+            if row['u1'] == 0 and row['u2'] == 0 and row['u3'] == 0 and row['u4'] == 0 and row['u5'] == 0 and row['u6'] == 0:
+                new_data = pd.DataFrame(current_segment)
+                new_data = new_data[(new_data['u1'] != 0) | (new_data['u2'] != 0) | (new_data['u3'] != 0) | (new_data['u4'] != 0) | (new_data['u5'] != 0) | (new_data['u6'] != 0)]
+                new_data = new_data.iloc[:500] # keep only 500 rows as in sim datasets
+                segments.append(new_data)
+
+                print(f"Segment #{num_test_files} with {len(segments[-1])} rows")
+
+                num_test_files, count, current_segment = num_test_files + 1, 0, []
+
+        current_segment.append(row) 
+        count += 1
+        
+        if num_test_files == 10:
+            break
+
+    return segments
 
 
 def add_virtual_node_at_origin(df):
@@ -43,7 +78,7 @@ def add_virtual_node_at_origin(df):
     df['z1'] = 0.1 # Will be swapped with y1 later
 
     return df
-            
+
 
 def rename_columns(data):
     data.rename(columns={col: input_mapping_from_real_to_sim[col] for col in data.columns if col.startswith("phi")}, inplace=True)
@@ -56,16 +91,25 @@ def rename_columns(data):
 
     return data
 
-def process_data(data):
-    data = add_virtual_node_at_origin(data)
-    # remove columns that are not needed
-    data['t'] = data['ID'] * 0.01
-    data = rename_columns(data)
+
+def filter_data(df):
+    filter_func = NonCausalTrunkFilter(num_nodes=4, measurement_noise=1e-1, position_process_noise=0, velocity_process_noise=1e1)
+    real_data_array = get_data_array_from_dataframe(df)[:, :, 0::2]
+    filtered_data = integrate_positions_from_velocity(real_data_array, filter_func.update_from_array(real_data_array))
+    return get_dataframe_from_data_array(filtered_data, join_with=df)
+
+
+def process_data(data, args=None):
+    # Reset indexes of data
+    data.reset_index(drop=True, inplace=True)
 
     # compute velocity
     for col in data.columns:
         if col.startswith(("x", "y", "z")):
             data[f"v{col}"] = data[col].diff() / 0.01 # use filter insteads
+    
+    if args.filter_data:
+        data = filter_data(data)
 
     # compute new states
     for col in data.columns:
@@ -75,45 +119,18 @@ def process_data(data):
     data = data.iloc[1:-1] # remove first and last row
     data = data[(data['u1'] != 0) | (data['u2'] != 0) | (data['u3'] != 0) | (data['u4'] != 0) | (data['u5'] != 0) | (data['u6'] != 0)]
     data = data.drop(columns=[col for col in data.columns if not col.startswith(("t", "x", "y", "z", "u", "v"))])
+    data.dropna() # remove idle states
+    data = data[sorted(data.columns)] # Sort columns alphabetically
 
-    # remove idle states
-
-    data.dropna()
-
-    # Sort columns alphabetically
-    data = data[sorted(data.columns)]
     
     return data
 
-def split_test_data(data):
-    segments = []
-    current_segment = []
-    count = 0
-
-    num_test_files = 0
-    for _, row in data.iterrows():
-        if count > 1000:
-            if row['u1'] == 0 and row['u2'] == 0 and row['u3'] == 0 and row['u4'] == 0 and row['u5'] == 0 and row['u6'] == 0:
-                new_data = pd.DataFrame(current_segment)
-                new_data = new_data[(new_data['u1'] != 0) | (new_data['u2'] != 0) | (new_data['u3'] != 0) | (new_data['u4'] != 0) | (new_data['u5'] != 0) | (new_data['u6'] != 0)]
-                new_data = new_data.iloc[:500] # keep only 500 rows as in sim datasets
-                segments.append(new_data)
-
-                print(f"Segment #{num_test_files} with {len(segments[-1])} rows")
-                num_test_files, count, current_segment = num_test_files + 1, 0, []
-
-        current_segment.append(row) 
-        count += 1
-        
-        if num_test_files == 10:
-            break
-
-    return segments
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate dataset from real measurements")
     parser.add_argument("--folder", type=str, default="data/long_rollouts_100_max_amplitude_80_policy_harmonic_inputs", help="Folder containing the data")
     parser.add_argument("--type", type=str, default="train", help="Data type (train, test)")
+    parser.add_argument("--filter-data", action="store_true", help="Whether to filter the data or not")
     args = parser.parse_args()
 
     folder = os.path.join(args.folder, args.type)
@@ -124,12 +141,13 @@ if __name__ == "__main__":
 
     if args.type == "test":
         data_segments = split_test_data(data)
+
         for i, data_segment in enumerate(tqdm(data_segments)):
             os.makedirs(os.path.join(folder, str(i+1), "raw"), exist_ok=True)
-            process_data(data_segment).to_csv(os.path.join(folder, str(i+1), "raw", "data.csv"), index=False)
+            process_data(data_segment, args).to_csv(os.path.join(folder, str(i+1), "raw", "data.csv"), index=False)
     else:
         os.makedirs(os.path.join(folder, "raw"), exist_ok=True)
-        process_data(data).to_csv(os.path.join(folder, "raw", "data.csv"), index=False)
+        process_data(data, args).to_csv(os.path.join(folder, "raw", "data.csv"), index=False)
 
     print(f"Data saved!")
     
