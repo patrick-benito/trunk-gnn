036dae33831e4396b25ae895a814e84e64f2ae00
Patrick Benito <peberhard@ethz.ch>
2025-05-16 17:37:10 -0700

    update gitignore and git automation

--- git_automation.sh
+++ git_automation.sh
@@ -10,16 +10,16 @@ git fetch origin main
 git pull
 
 # Get latest commit hash
-LATEST_COMMIT_HASH=$(git rev-parse origin/main)
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+LATEST_COMMIT_HASH=$(git rev-parse $CURRENT_BRANCH)
 LAST_COMMIT_HASH=$(cat $LAST_COMMIT_FILE 2>/dev/null)
 
 # Check if new commit exists
 if [ "$LATEST_COMMIT_HASH" != "$LAST_COMMIT_HASH" ]; then
-    LATEST_COMMIT_MSG=$(git log -1 --pretty=%B origin/main)
+    LATEST_COMMIT_MSG=$(git log -1 --pretty=%B $CURRENT_BRANCHn)
 
     if [[ "$LATEST_COMMIT_MSG" == *"[TRAIN]"* ]]; then
         echo "New [TRAIN] commit found. Running training..."
-        git pull origin main
         nohup /usr/bin/python3 -m uv run $TRAIN_SCRIPT > train.log 2>&1 &
     fi
     # Update last processed commit
